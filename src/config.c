40e5c9521ce669d90ecfb6175b6c372f2289ebf5
John Bredall <bredall.1@buckeyemail.osu.edu>
2021-10-12 00:37:22 -0400

    refactor: remove unused include

--- src/config.c
+++ src/config.c
@@ -6,8 +6,8 @@ void config_initialize() {
 	cfg.files.max_bri = "/sys/class/backlight/acpi_video0/max_brightness";
 	cfg.files.plug_state = "/sys/class/power_supply/ADP1/online";
 	cfg.files.lid_state = "/proc/acpi/button/lid/LID0/state";
-	cfg.files.als_lux = "/sys/bus/iio/devices/iio:device0/in_illuminance_input"; 
-	cfg.files.als_freq = "/sys/bus/iio/devices/iio:device0/in_illuminance_sampling_frequency"; 
+	cfg.files.als_lux = "/sys/bus/iio/devices/iio:device0/in_illuminance_input";
+	cfg.files.als_freq = "/sys/bus/iio/devices/iio:device0/in_illuminance_sampling_frequency";
 
 	cfg.scales.min_lux = 50;
 	cfg.scales.max_lux = 1000000;
