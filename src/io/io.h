777c335ea315325115349849fba715140d03d2fe
John Bredall <bredall.1@buckeyemail.osu.edu>
2021-10-13 13:21:53 -0400

    fix: added new missing declarations

--- src/io/io.h
+++ src/io/io.h
@@ -8,5 +8,7 @@
 extern int read_str(char * file_name, char var[BUFF_SIZE]);
 extern int write_int(char * file_name, int input);
 extern int read_int(char * file_name, int * var);
+extern int read_long(char * file_name, unsigned long int * var);
+extern int read_float(char * file_name, float * var);
 
 #endif
